e132d6aa9a93f30b7d537c1503833a5105e9f4c1
Esotuofgr7 <kachiesotu@gmail.com>
2025-08-10 19:18:44 +0100

    Add files via upload

---
+++ set_union.py
@@ -0,0 +1,12 @@
+setc1 = {"red", "blue"}
+setc2 = {"blue", "green"}
+
+print("Original Sets:")
+print(setc1)
+print(setc2)
+
+# Perform union and store the result
+setc = setc1.union(setc2)
+
+print("\nUnion of above sets:")
+print(setc)
